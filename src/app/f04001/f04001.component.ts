dca8e35dc26d9bdaddf37f60915bf07735b67045
YungHsing <x70343@gmail.com>
2020-12-23 18:33:37 +0800

    learn

--- src/app/f04001/f04001.component.ts
+++ src/app/f04001/f04001.component.ts
@@ -1,4 +1,9 @@
+import { AfterViewInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { F04001Service } from './f04001.service';
 
 interface Food {
@@ -11,19 +16,59 @@ interface Food {
   templateUrl: './f04001.component.html',
   styleUrls: ['./f04001.component.css']
 })
-export class F04001Component implements OnInit {
-
+export class F04001Component implements OnInit, AfterViewInit {
+  foods: Food[] = [];
+  selectedValue: string;
   constructor(private f04001Service: F04001Service) { }
-
   ngOnInit(): void {
+    this.f04001Service.getSysTypeCode().subscribe(data => {
+      for (const jsonObj of data) {
+        const codeNo = jsonObj['code_NO'];
+        const desc = jsonObj['code_DESC'];
+        this.foods.push({value: codeNo, viewValue: desc})
+      }
+    });
+  }
+//============================================================
+  totalCount: any;
+  @ViewChild('paginator', { static: true }) paginator: MatPaginator;
+  @ViewChild('sortTable', { static: true }) sortTable: MatSort;
+  currentPage: PageEvent;
+  currentSort: Sort;
+  mappingCodeSource = new MatTableDataSource<any>();
+  ngAfterViewInit() {
+    this.currentPage = {
+      pageIndex: 0,
+      pageSize: 10,
+      length: null
+    };
+    this.currentSort = {
+      active: '',
+      direction: ''
+    };
+    this.getMappingCode();
+    this.paginator.page.subscribe((page: PageEvent) => {
+      this.currentPage = page;
+      this.getMappingCode();
+    });
   }
 
-  selectedValue: string;
+  changeSort(sortInfo: Sort) {
+    this.currentSort = sortInfo;
+    this.getMappingCode();
+  }
 
-  foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'}
-  ];
+  getMappingCode() {
+    this.f04001Service.getMappingCode(this.currentPage.pageIndex, this.currentPage.pageSize, this.selectedValue)
+    .subscribe(data => {
+      this.totalCount = data.size;
+      this.mappingCodeSource.data = data.items;
+    });
+  }
 
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.mappingCodeSource.filter = filterValue;
+  }
 }
--- src/app/f04001/f04001.service.ts
+++ src/app/f04001/f04001.service.ts
@@ -8,9 +8,14 @@ import { BaseService } from '../base.service';
 })
 export class F04001Service extends BaseService {
   constructor(protected httpClient: HttpClient) { super(); }
-  getCaseList(pageIndex: number, pageSize: number): Observable<any> {
-    const baseUrl = 'http://localhost:8080/getCaseList';
-    let targetUrl = `${baseUrl}?page=${pageIndex + 1}&per_page=${pageSize}`;
+  getSysTypeCode(): Observable<any> {
+    const baseUrl = 'http://localhost:8080/getSysTypeCode';
+    return this.httpClient.post<any>(baseUrl, this.httpOptions);
+  }
+
+  getMappingCode(pageIndex: number, pageSize: number, sysType: String): Observable<any> {
+    const baseUrl = 'http://localhost:8080/getMappingCode';
+    let targetUrl = `${baseUrl}?page=${pageIndex + 1}&per_page=${pageSize}&sys_type=BRANCH_CODE`;
     return this.httpClient.post<any>(targetUrl, this.httpOptions);
   }
 }
