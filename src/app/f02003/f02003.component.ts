81bfc86881e0712938853c679e66d42d1c93e7a0
YungHsing <x70343@gmail.com>
2021-04-13 16:57:47 +0800

    調整路由

---
+++ src/app/f02003/f02003.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-f02003',
+  templateUrl: './f02003.component.html',
+  styleUrls: ['./f02003.component.css']
+})
+export class F02003Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/f02004/f02004.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-f02004',
+  templateUrl: './f02004.component.html',
+  styleUrls: ['./f02004.component.css']
+})
+export class F02004Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
