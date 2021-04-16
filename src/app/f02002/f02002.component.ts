5bf0aed7a6661bf5ec64d6966adeeafcfdd37b72
omar10218 <omar10218@gmail.com>
2021-04-16 14:38:52 +0800

    f02002Date

--- src/app/f02002/f02002.component.ts
+++ src/app/f02002/f02002.component.ts
@@ -15,18 +15,31 @@ interface COMB {
 export class F02002Component implements OnInit {
   dateStart = '1999-01-01';
   //之後API取得下拉內容
-  nation: COMB[] = [{value: 'Tai', viewValue: 'Taiwan'}, {value: 'Jan', viewValue: 'Japan'}, {value: 'USA', viewValue: 'USA'}];
-  gender: COMB[] = [{value: 'M', viewValue: 'Male'}, {value: 'F', viewValue: 'Female'}];
+  nationCode: COMB[] = [{value: 'Tai', viewValue: 'Taiwan'}, {value: 'Jan', viewValue: 'Japan'}, {value: 'USA', viewValue: 'USA'}];
+  genderCode: COMB[] = [{value: 'M', viewValue: 'Male'}, {value: 'F', viewValue: 'Female'}];
+
+  planModel: any = {start_time: new Date() };
+
+  date: string = '';
+
+  dateChanged(evt) {
+    let selectedDate = new Date(evt);
+    console.log("by default:", selectedDate);
+    console.log("by UTCString:", selectedDate.toUTCString());
+    console.log("by LocaleString:", selectedDate.toLocaleString());
+    console.log("by LocaleTimeString:", selectedDate.toLocaleTimeString());
+    this.date = selectedDate.toUTCString.toString();
+  }
 
   registrationForm: FormGroup = this.fb.group({
-    dncert: ['', [Validators.maxLength(30)]],
-    namecert: ['', [Validators.required, Validators.maxLength(50)]],
-    idnumbercert: ['', [Validators.required, Validators.maxLength(10)]],
-    natcert: ['tai', [Validators.required, Validators.maxLength(3)]],
-    gencert: ['m', [Validators.required, Validators.maxLength(1)]],
-    birthdatecert: ['', [Validators.required, Validators.maxLength(10)]],
-    phonenumbercert: ['', [Validators.required, Validators.maxLength(30)]],
-    addresscert: ['', [Validators.required, Validators.maxLength(128)]]
+    dn: ['', [Validators.maxLength(30)]],
+    name: ['', [Validators.required, Validators.maxLength(50)]],
+    idNumber: ['', [Validators.required, Validators.maxLength(10)]],
+    nation: ['tai', [Validators.required, Validators.maxLength(3)]],
+    gender: ['m', [Validators.required, Validators.maxLength(1)]],
+    birthDate: ['', [Validators.required, Validators.maxLength(10)]],
+    phoneNumber: ['', [Validators.required, Validators.maxLength(30)]],
+    address: ['', [Validators.required, Validators.maxLength(128)]]
   });
 
   submitted = false;
@@ -50,6 +63,7 @@ export class F02002Component implements OnInit {
   onSubmit() {
     this.submitted = true;
     console.log(FormGroup);
+    this.registrationForm.patchValue({birthDate: this.date});
     if(!this.registrationForm.valid) {
       alert('資料必填喔!')
       return false;
