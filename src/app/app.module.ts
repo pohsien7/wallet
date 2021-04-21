2ae378639871089886526a7cad6377a3109937ee
Paul <pohsien.lu@rstn.com.tw>
2021-04-21 17:12:05 +0800

    debug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuListModule } from './menu-list/menu-list.module';
@@ -12,11 +13,14 @@ import { BlockUIModule } from 'ng-block-ui';
 
 import { F04001Component } from './f04001/f04001.component';
 import { F04002Component } from './f04002/f04002.component';
+import { F04003Component } from './f04003/f04003.component';
 import { F05001Component } from './f05001/f05001.component';
 import { F05002Component } from './f05002/f05002.component';
 
 import { F01001Component } from './f01001/f01001.component';
 
+// import { AddComponent } from './f04001/f04001add/add.component';
+// import { EditComponent } from './f04001/f04001edit/edit.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BnNgIdleService } from 'bn-ng-idle';
 
@@ -40,6 +44,8 @@ import { F06004editComponent } from './f06004/f06004edit/f06004edit.component';
 import { F06004deleteComponent } from './f06004/f06004delete/f06004delete.component';
 
 import { MaterialModule } from './material/material.module';
+// import { F04002addComponent } from './f04002/f04002add/f04002add.component';
+// import { F04002editComponent } from './f04002/f04002edit/f04002edit.component';
 import { F04001confirmComponent } from './f04001/f04001confirm/f04001confirm.component';
 import { F04002confirmComponent } from './f04002/f04002confirm/f04002confirm.component';
 import { F01002Component } from './f01002/f01002.component';
@@ -62,6 +68,30 @@ import { F07009Component } from './f07009/f07009.component';
 import { F02003Component } from './f02003/f02003.component';
 import { F02004Component } from './f02004/f02004.component';
 import { F02001confirmComponent } from './f02001/f02001confirm/f02001confirm.component';
+import { MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { F02005Component } from './f02005/f02005.component';
+import { F02006Component } from './f02006/f02006.component';
+import { F02005confirmComponent } from './f02005/f02005confirm/f02005confirm.component';
+import { F02005wopenComponent } from './f02005/f02005wopen/f02005wopen.component';
+import { F02006confirmComponent } from './f02006/f02006confirm/f02006confirm.component';
+import { F02006wopenComponent } from './f02006/f02006wopen/f02006wopen.component';
+import { CssSettingComponent } from './css-setting/css-setting.component';
+import { MAT_COLOR_FORMATS, NgxMatColorPickerModule, NGX_MAT_COLOR_FORMATS } from '@angular-material-components/color-picker';
+import { F03001confirmComponent } from './f03001/f03001confirm/f03001confirm.component';
+import { F03002confirmComponent } from './f03002/f03002confirm/f03002confirm.component';
+
+
+export const TW_FORMATS = {
+  parse: {
+    dateInput: 'YYYY/MM/DD'
+  },
+  display: {
+    dateInput: 'YYYY/MM/DD',
+    monthYearLabel: 'YYYY MMM',
+    dateA11yLabel: 'YYYY/MM/DD',
+    monthYearA11yLabel: 'YYYY MMM'
+  }
+}
 
 @NgModule({
   declarations: [
@@ -69,11 +99,13 @@ import { F02001confirmComponent } from './f02001/f02001confirm/f02001confirm.com
     HomeComponent,
     F04001Component,
     F04002Component,
+    F04003Component,
     F05001Component,
     F05002Component,
     LoginComponent,
-
     F01001Component,
+    // AddComponent,
+    // EditComponent,
     F06001Component,
     F06002Component,
     F06003Component,
@@ -91,6 +123,8 @@ import { F02001confirmComponent } from './f02001/f02001confirm/f02001confirm.com
     F06004addComponent,
     F06004editComponent,
     F06004deleteComponent,
+    // F04002addComponent,
+    // F04002editComponent,
     F04001confirmComponent,
     F04002confirmComponent,
     F01002Component,
@@ -112,7 +146,15 @@ import { F02001confirmComponent } from './f02001/f02001confirm/f02001confirm.com
     F07009Component,
     F02003Component,
     F02004Component,
-    F02001confirmComponent
+    F02001confirmComponent,
+    F02005Component,
+    F02006Component,
+    F02005confirmComponent,
+    F02005wopenComponent,
+    F02006confirmComponent,
+    F02006wopenComponent,
+    F03001confirmComponent,
+    F03002confirmComponent
   ],
   imports: [
     BrowserModule,
@@ -125,9 +167,16 @@ import { F02001confirmComponent } from './f02001/f02001confirm/f02001confirm.com
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
-    BlockUIModule.forRoot()
+    BlockUIModule.forRoot(),
+    NgxMatColorPickerModule
   ],
-  providers: [BnNgIdleService],
+  providers: [
+    BnNgIdleService,
+    DatePipe,
+    { provide: MAT_DATE_LOCALE, useValue: 'zh-TW' },
+    { provide: MAT_DATE_FORMATS, useValue: TW_FORMATS },
+    { provide: MAT_COLOR_FORMATS, useValue: NGX_MAT_COLOR_FORMATS }
+  ],
   bootstrap: [AppComponent]
 })
 
