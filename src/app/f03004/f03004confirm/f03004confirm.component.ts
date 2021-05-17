8853d8fe9b319f179ac4cdb26c0b1fa6d565a900
Mount.Lin <mount.lin@rstn.com.tw>
2021-05-17 16:20:01 +0800

    新增終端用戶申請驗證基碼

--- src/app/f03004/f03004confirm/f03004confirm.component.ts
+++ src/app/f03004/f03004confirm/f03004confirm.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   templateUrl: './f03004confirm.component.html',
   styleUrls: ['./f03004confirm.component.css']
 })
 export class F03004confirmComponent implements OnInit {
-
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<F03004confirmComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
+  
 
   ngOnInit(): void {
   }
