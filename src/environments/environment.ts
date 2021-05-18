54ab328dd38b03baff2f729d3715977d13dbe987
YungHsing <x70343@gmail.com>
2021-05-18 18:15:46 +0800

    修改url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://my-prod-url',
-  allowOrigin: 'http://localhost:8080'
+  allowOrigin: 'http://localhost:8080/cdbc-web'
   //allowOrigin: 'http://192.168.0.62:8080'
 };
