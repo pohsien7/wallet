5939e7434f5355e2928baebb9c5eb501ec4f4cc6
YungHsing <x70343@gmail.com>
2021-05-18 11:26:00 +0800

    新增qrcode功能

---
+++ src/app/qrcode/qrcodeconfirm/qrcodeconfirm.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './qrcodeconfirm.component.html',
+  styleUrls: ['./qrcodeconfirm.component.css']
+})
+export class QrcodeconfirmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
