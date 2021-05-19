249ddc14e6eab81e8fd1c18a8c0fc96204dac221
YungHsing <x70343@gmail.com>
2021-05-19 19:15:04 +0800

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'http://my-prod-url',
-  allowOrigin: 'http://127.0.0.1:8080/cbdc-web'
+  allowOrigin: 'http://192.168.0.34:8080/cbdc-web'
 };
