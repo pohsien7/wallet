8f8d68d567f142af61301ba708c94964c8949ce3
YungHsing <x70343@gmail.com>
2021-05-21 10:29:10 +0800

    修改+新增

---
+++ src/app/f03001/f03001wopen/f03001wopen.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './f03001wopen.component.html',
+  styleUrls: ['./f03001wopen.component.css']
+})
+export class F03001wopenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
