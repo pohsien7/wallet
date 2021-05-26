094227f891bee6877bb9cbdf15ace90e736348c5
Paul <pohsien.lu@rstn.com.tw>
2021-05-26 14:17:31 +0800

    qrcode

--- src/app/qrcode/qrcode.service.ts
+++ src/app/qrcode/qrcode.service.ts
@@ -1,9 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BaseService } from '../base.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class QrcodeService {
-
-  constructor() { }
+export class QrcodeService extends BaseService {
+  constructor(protected httpClient: HttpClient) { super(httpClient); }
 }
+
