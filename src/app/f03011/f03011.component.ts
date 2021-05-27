9b8fb19506bec25c19a28baea8d47d7e6c2f4882
Mount.Lin <mount.lin@rstn.com.tw>
2021-05-27 17:51:36 +0800

    20210527新增交易ID查詢自錢包帳本記錄功能

--- src/app/f03011/f03011.component.ts
+++ src/app/f03011/f03011.component.ts
@@ -1,15 +1,123 @@
-import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { F03011Service } from './f03011.service';
+import { F03011confirmComponent } from './f03011confirm/f03011confirm.component';
 
+interface COMB {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-f03011',
   templateUrl: './f03011.component.html',
-  styleUrls: ['./f03011.component.css']
+  styleUrls: ['./f03011.component.css', '../../assets/css/f04.css']
 })
 export class F03011Component implements OnInit {
+  
+  @BlockUI() blockUI: NgBlockUI;
 
-  constructor() { }
+  cvcCode: COMB[] = [{ value: '0901', viewValue: '0901' }];
+
+  // 驗證範例 => https://stackblitz.com/edit/full-angular-reactive-forms-demo?file=src%2Fapp%2Fapp.component.ts
+  queryWalletLedgerForm: FormGroup = this.fb.group({
+    walletID: ['', [Validators.maxLength(25)]],
+    queryTxnID: ['', []],
+    cvc: ['', [Validators.maxLength(4)]]
+  });
+
+  resultForm: FormGroup = this.fb.group({
+    txnID : ['', []],
+    senderID: ['', []],
+    recipientID: ['', []],
+    authorizedAgencyID: ['', []],
+    recipientDN: ['', []],
+    cvc: ['', []],
+    amount: ['', []],
+    balance: ['', []],
+    sn: ['', []],
+    won: ['', []],
+    remark: ['', []],
+    txnTime: ['', []],
+    result: ['', []],
+    paymentMethod: ['', []]
+  });
+
+  submitted = false;
+  constructor(private fb: FormBuilder, public f03011Service: F03011Service, public dialog: MatDialog) { }
 
   ngOnInit(): void {
+
   }
 
+  formControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  getErrorMessage() {
+    return this.formControl.hasError('required') ? 'Required field' :
+    '';
+  }
+
+  isFieldEmpty() {
+    if(this.queryWalletLedgerForm.value.walletID == '' && this.queryWalletLedgerForm.value.queryTxnID == ''
+      && this.queryWalletLedgerForm.value.cvc =='' 
+    ) {
+      return true;
+    }
+  }
+
+  async sendCBDC() {
+    let msg = '';
+    this.submitted = true;
+    this.blockUI.start('Loading...');
+    if(!this.queryWalletLedgerForm.valid) {
+      msg = '資料格式有誤，請修正!'
+    } else {
+      const formdata: FormData = new FormData();
+      formdata.append('value', JSON.stringify(this.queryWalletLedgerForm.value));
+      await this.f03011Service.sendConsumer('consumer/f03011', formdata).then((data) => {
+        if (data.statusCode == "0000") {
+          this.resultForm.patchValue({ txnID : data.ledgerState.txnID });
+          this.resultForm.patchValue({ senderID : data.ledgerState.senderID });
+          this.resultForm.patchValue({ recipientID : data.ledgerState.recipientID });
+          this.resultForm.patchValue({ authorizedAgencyID : data.ledgerState.authorizedAgencyID });
+          this.resultForm.patchValue({ recipientDN : data.ledgerState.recipientDN });
+          this.resultForm.patchValue({ cvc : data.ledgerState.cvc });
+          this.resultForm.patchValue({ amount : data.ledgerState.amount });
+          this.resultForm.patchValue({ balance : data.ledgerState.balance });
+          this.resultForm.patchValue({ sn : data.ledgerState.sn });
+          this.resultForm.patchValue({ won : data.ledgerState.won });
+          this.resultForm.patchValue({ remark : data.ledgerState.remark });
+          this.resultForm.patchValue({ txnTime : data.ledgerState.txnTime });
+          this.resultForm.patchValue({ result : data.ledgerState.result });
+          this.resultForm.patchValue({ paymentMethod : data.ledgerState.paymentMethod });
+        } else {
+          msg = data.statusMessage;
+        }
+      });
+    }
+    setTimeout(() => {
+      this.blockUI.stop(); // Stop blocking
+      const childernDialogRef = this.dialog.open(F03011confirmComponent, { data: { msgStr: msg } });
+    }, 3000);
+  }
+
+  setTimes() {
+    if (this.queryWalletLedgerForm.value.txntime_end == null) {
+      this.queryWalletLedgerForm.patchValue({txntime_end:this.queryWalletLedgerForm.value.txntime_start});
+    }
+  }
+
+  clear() {
+    this.queryWalletLedgerForm.patchValue({
+      walletID:'', queryTxnID:'', cvc:''
+    });
+  }
+    
 }
