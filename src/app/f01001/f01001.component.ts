d8c04d0ae3644ab1e079e14319f8dba9bcfcb773
omar10218 <omar10218@gmail.com>
2021-05-28 15:17:57 +0800

    新增CSS

--- src/app/f01001/f01001.component.ts
+++ src/app/f01001/f01001.component.ts
@@ -14,7 +14,7 @@ interface COMB {
 @Component({
   selector: 'app-f01001',
   templateUrl: './f01001.component.html',
-  styleUrls: ['./f01001.component.css']
+  styleUrls: ['./f01001.component.css','../../assets/css/f01.css']
 })
 export class F01001Component implements OnInit {
   @BlockUI() blockUI: NgBlockUI;
