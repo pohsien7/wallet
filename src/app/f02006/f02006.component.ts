9bd849ec8bd7221342cf30eb62208b21a487f428
omar10218 <omar10218@gmail.com>
2021-06-09 15:32:00 +0800

    微調

--- src/app/f02006/f02006.component.ts
+++ src/app/f02006/f02006.component.ts
@@ -9,7 +9,7 @@ import { F02006wopenComponent } from './f02006wopen/f02006wopen.component';
 @Component({
   selector: 'app-f02006',
   templateUrl: './f02006.component.html',
-  styleUrls: ['./f02006.component.css','../../assets/css/f02.css']
+  styleUrls: ['./f02006.component.css', '../../assets/css/f02.css']
 })
 export class F02006Component implements OnInit {
 
@@ -21,7 +21,7 @@ export class F02006Component implements OnInit {
   });
 
   resultForm: FormGroup = this.fb.group({
-    senderID : ['', []],
+    senderID: ['', []],
     recipientID: ['', []],
     authorizedPartyID: ['', []],
     recipientDN: ['', []],
@@ -53,37 +53,39 @@ export class F02006Component implements OnInit {
 
   async getErrorMessage(cloumnName: string) {
     let obj = this.registrationForm.get(cloumnName);
-    return obj.hasError('required')  ? '此為必填欄位!' : obj.hasError('maxlength') ? '長度過長' :
-           obj.hasError('minlength') ? '長度過短' : '';
+    return obj.hasError('required') ? '此為必填欄位!' : obj.hasError('maxlength') ? '長度過長' :
+      obj.hasError('minlength') ? '長度過短' : '';
   }
 
   async sendCBDC() {
     let msg = '';
     this.submitted = true;
     this.blockUI.start('Loading...');
-    if(!this.registrationForm.valid) {
+    if (!this.registrationForm.valid) {
       msg = '資料格式有誤，請修正!'
     } else {
       const formdata: FormData = new FormData();
       formdata.append('value', JSON.stringify(this.registrationForm.value));
       await this.f02006Service.sendConsumer('consumer/f02006', formdata).then((data) => {
         msg = data.statusMessage;
-        this.resultForm.patchValue({ senderID : data.ledgerState.senderID });
-        this.resultForm.patchValue({ recipientID : data.ledgerState.recipientID });
-        this.resultForm.patchValue({ authorizedPartyID : data.ledgerState.authorizedPartyID });
-        this.resultForm.patchValue({ recipientDN : data.ledgerState.recipientDN });
-        this.resultForm.patchValue({ cvc : data.ledgerState.cvc });
-        this.resultForm.patchValue({ amount : data.ledgerState.amount });
-        this.resultForm.patchValue({ senderBalance : data.ledgerState.senderBalance });
-        this.resultForm.patchValue({ recipientBalance : data.ledgerState.recipientBalance });
-        this.resultForm.patchValue({ senderSN : data.ledgerState.senderSN });
-        this.resultForm.patchValue({ recipientSN : data.ledgerState.recipientSN });
-        this.resultForm.patchValue({ won : data.ledgerState.won });
-        this.resultForm.patchValue({ remark : data.ledgerState.remark });
-        this.resultForm.patchValue({ txnTime : data.ledgerState.txnTime });
-        this.resultForm.patchValue({ result : data.ledgerState.result });
-        this.resultForm.patchValue({ paymentMethod : data.ledgerState.paymentMethod });
-        this.resultForm.patchValue({ insID : data.ledgerState.insID });
+        if (data.statusMessage == 'Success') {
+          this.resultForm.patchValue({ senderID: data.ledgerState.senderID });
+          this.resultForm.patchValue({ recipientID: data.ledgerState.recipientID });
+          this.resultForm.patchValue({ authorizedPartyID: data.ledgerState.authorizedPartyID });
+          this.resultForm.patchValue({ recipientDN: data.ledgerState.recipientDN });
+          this.resultForm.patchValue({ cvc: data.ledgerState.cvc });
+          this.resultForm.patchValue({ amount: data.ledgerState.amount });
+          this.resultForm.patchValue({ senderBalance: data.ledgerState.senderBalance });
+          this.resultForm.patchValue({ recipientBalance: data.ledgerState.recipientBalance });
+          this.resultForm.patchValue({ senderSN: data.ledgerState.senderSN });
+          this.resultForm.patchValue({ recipientSN: data.ledgerState.recipientSN });
+          this.resultForm.patchValue({ won: data.ledgerState.won });
+          this.resultForm.patchValue({ remark: data.ledgerState.remark });
+          this.resultForm.patchValue({ txnTime: data.ledgerState.txnTime });
+          this.resultForm.patchValue({ result: data.ledgerState.result });
+          this.resultForm.patchValue({ paymentMethod: data.ledgerState.paymentMethod });
+          this.resultForm.patchValue({ insID: data.ledgerState.insID });
+        }
       });
     }
     setTimeout(() => {
@@ -100,21 +102,21 @@ export class F02006Component implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       if (result != null && result.event == 'success') {
         this.display = false;
-        this.registrationForm.patchValue({ queryTxnID : result.value });
-        this.registrationForm.patchValue({ cvc : result.cvc });
+        this.registrationForm.patchValue({ queryTxnID: result.value });
+        this.registrationForm.patchValue({ cvc: result.cvc });
       }
     });
   }
 
-  getCvc(){
-    if ( this.registrationForm.value.queryTxnID.length == 42 ) {
-      this.f02006Service.get("RM",this.registrationForm.value.queryTxnID).then((data) =>{
+  getCvc() {
+    if (this.registrationForm.value.queryTxnID.length == 42) {
+      this.f02006Service.get("RM", this.registrationForm.value.queryTxnID).then((data) => {
         console.log(data);
-        if ( data.IDerror == "error"){
+        if (data.IDerror == "error") {
           this.display = true;
         } else {
           this.display = false;
-          this.registrationForm.patchValue({ cvc: data.CVC});
+          this.registrationForm.patchValue({ cvc: data.CVC });
         }
       });
     }
