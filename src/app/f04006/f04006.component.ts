cec5b0968f49985f14988398f6fed8a073b1f3db
YungHsing <x70343@gmail.com>
2021-06-10 16:32:30 +0800

    add f04006

---
+++ src/app/f04006/f04006.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-f04006',
+  templateUrl: './f04006.component.html',
+  styleUrls: ['./f04006.component.css']
+})
+export class F04006Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
