c74669d9f9a1c8235f8641912cbe768ba38d0c18
omar10218 <omar10218@gmail.com>
2021-06-15 20:30:39 +0800

    查詢

--- src/app/f01005/f01005confirm/f01005confirm.component.ts
+++ src/app/f01005/f01005confirm/f01005confirm.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-f01005confirm',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class F01005confirmComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<F01005confirmComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit(): void {
   }
