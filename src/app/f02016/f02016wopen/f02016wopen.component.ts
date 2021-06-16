5f8af172c0ef9f741736f3dad403928c62cb6e72
omar10218 <omar10218@gmail.com>
2021-06-16 12:49:20 +0800

    更新公鑰

--- src/app/f02016/f02016wopen/f02016wopen.component.ts
+++ src/app/f02016/f02016wopen/f02016wopen.component.ts
@@ -1,14 +1,116 @@
-import { Component, OnInit } from '@angular/core';
-
+import { DatePipe } from '@angular/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { F02016Service } from '../f02016.service';
+interface sysCode {
+  value: string;
+  viewValue: string;
+}
 @Component({
   templateUrl: './f02016wopen.component.html',
   styleUrls: ['./f02016wopen.component.css']
 })
 export class F02016wopenComponent implements OnInit {
 
-  constructor() { }
+  searchForm: FormGroup = this.fb.group({
+    walletType: ['', [Validators.required]],
+    startTime: ['', [ ]],
+    endTime: ['', [ ]],
+    dn: ['', [ ]],
+    page: ['', [ ]],
+    perPage: ['', [ ]]
+  });
+
+  walletOption: sysCode[] = [
+    {value: 'NPWALLET_PUBKEY', viewValue: '記名錢包 (自然人，公鑰)'},
+    {value: 'ANONYMOUS_WALLET', viewValue: '匿名錢包'}];
+
+  constructor(public dialogRef: MatDialogRef<F02016wopenComponent>, private fb: FormBuilder, private datePipe: DatePipe, private f02016Service: F02016Service) { }
 
   ngOnInit(): void {
   }
 
+  totalCount: any;
+  @ViewChild('paginator', { static: true }) paginator: MatPaginator;
+  @ViewChild('sortTable', { static: true }) sortTable: MatSort;
+  currentPage: PageEvent;
+  currentSort: Sort;
+  walletIdSource = new MatTableDataSource<any>();
+
+  ngAfterViewInit() {
+    this.currentPage = {
+      pageIndex: 0,
+      pageSize: 10,
+      length: null,
+
+    };
+    this.currentSort = {
+      active: '',
+      direction: ''
+    };
+    this.paginator.page.subscribe((page: PageEvent) => {
+      this.currentPage = page;
+      this.getWalletId();
+    });
+  }
+
+  cleanToEmpty() {
+    this.searchForm.patchValue({ dn : '' });
+    this.searchForm.patchValue({ walletType : '' });
+    this.searchForm.patchValue({ startTime : '' });
+    this.searchForm.patchValue({ endTime : '' });
+    this.currentPage = {
+      pageIndex: 0,
+      pageSize: 10,
+      length: null
+    };
+    this.totalCount = 0;
+    this.paginator.firstPage();
+    this.walletIdSource.data = null;
+  }
+
+  async getWalletId() {
+    let jsonStr :string = JSON.stringify(this.searchForm.value);
+    let jsonObj = JSON.parse(jsonStr);
+    //1.處理日期
+    if (this.searchForm.value.startTime != '' && this.searchForm.value.endTime != '') {
+      let selectedStartDate = new Date(this.searchForm.value.startTime);
+      let selectedEndDate = new Date(this.searchForm.value.endTime);
+      jsonObj.startTime = this.datePipe.transform(selectedStartDate,"yyyy-MM-dd");
+      jsonObj.endTime = this.datePipe.transform(selectedEndDate,"yyyy-MM-dd");
+    }
+    //2.處理分頁
+    let page :string = `${this.currentPage.pageIndex + 1}`;
+    let perPage :string = `${this.currentPage.pageSize}`;
+    jsonObj.page = page;
+    jsonObj.perPage = perPage;
+    //3.轉回字串
+    let jsonString :string = JSON.stringify(jsonObj);
+    this.f02016Service.getWalletIdList('/consumer/f02016fn01', jsonString).subscribe(data => {
+      this.totalCount = data.size;
+      this.walletIdSource.data = data.items;
+    });
+  }
+
+  formControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  getErrorMessage() {
+    return this.formControl.hasError('required') ? '此為必填欄位!' : '';
+  }
+
+  setEndTimes() {
+    if (this.searchForm.value.startTime != null && this.searchForm.value.endTime == null) {
+      this.searchForm.patchValue({ endTime:this.searchForm.value.startTime });
+    }
+  }
+
+  goBack(walletID: string) {
+    this.dialogRef.close({ event:'success', walletID: walletID });
+  }
 }
